58779d3344eb055ec9079a66060428f02ce1b06d
otake <sheepdogs_2@hotmail.com>
2018-09-22 18:27:04 +0900

    第一講課題4rspec適用

--- lec001/fp01_04.rb
+++ lec001/fp01_04.rb
@@ -4,19 +4,17 @@ require 'bigdecimal/util'
 =begin
 誤差あり
 =end
-def calc(a,b,c)
+def calc_a(a,b,c)
 	begin
 		result = Array.new
-
 		if calc_d(a,b,c).negative? then
 			raise "解なし"
 		end
 		result.push((-b.to_f - Math.sqrt(calc_d(a,b,c).to_f)) / (2.0 * a.to_f))
 		result.push((-b.to_f + Math.sqrt(calc_d(a,b,c).to_f)) / (2.0 * a.to_f))
-
 		return result
 	rescue Exception => e
-		return e.message
+		return e
 	end
 end
 
@@ -25,29 +23,30 @@ def calc_d(a,b,c)
 	begin
 		return (b.to_f * b.to_f) - (4.to_f * a.to_f * c.to_f)
 	rescue Exception => e
-		return e.message
+		return e
 	end
 end
 
 =begin
-誤差なし
+c. 仮に b ≥ 0 とする (負なら a, b, c すべてに −1 を掛ければ解は同じで b ≥ 0 とできる)。す
+ると、± のうち − については両方の符号が同じなので桁落ちなしに解が求まる。これを α
+とおき、解と係数の関係 αβ =
+c
+a を利用して他方の解 β を求めることができる。この方法
+で 2 つの解を求めて打ち出す (または「[値, 値]」の形で返す) メソッドを作成しなさい。
+いくつかの値で実行例を示すこと。
 =end
-def calc2(a,b,c)
+def calc_c(a,b,c)
 	begin
 		result = Array.new
-
 		if calc_d(a,b,c).negative? then
 			raise "解なし"
 		end
-
-
-
-		if b > 0 then
+		if b >= 0 then
 			alpha = (b + Math.sqrt(calc_d2(a,b,c))) / (2.0 * a)
 			result.push(-alpha)
 			beta = c/(a * alpha)
 			result.push(-beta)
-			puts(result)
 		else
 			a = a * -1
 			b = b * -1
@@ -56,14 +55,8 @@ def calc2(a,b,c)
 			result.push(alpha)
 			beta = c/(a * alpha)
 			result.push(beta)
-			puts(alpha) 
 		end
-
 		beta = c/(a * alpha)
-
-		#result.push(alpha)
-		#result.push(beta)
-
 		return result
 	rescue Exception => e
 		return e.message
@@ -75,7 +68,7 @@ def calc_d2(a,b,c)
 	begin
 		return (b * b) - (4 * a * c)
 	rescue Exception => e
-		return e.message
+		return e
 	end
 end
 
