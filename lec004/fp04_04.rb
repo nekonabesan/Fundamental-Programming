c5291f46c48d1ef9687e6f4d7807f1870ce9b801
nekonabesan <sheepdogs_2@hotmail.com>
2018-10-02 21:41:56 +0900

    演習４、ソース修正

--- lec004/fp04_04.rb
+++ lec004/fp04_04.rb
@@ -20,12 +20,42 @@ a. 階乗の計算。
 =end
 def fact(n)
   begin
-    raise "n is Empty" if n == ''
-    raise "n is Null" if n == nil
-    raise "Out of Range" if 0 > n
+    raise ArgumentError if n.nil? || 0 > n
     return 1 if n == 0
     return n = n * fact(n - 1)
   rescue => e
     return e
   end
+end
+
+=begin
+b. フィボナッチ数。
+=end
+def fib(n)
+  begin
+    raise ArgumentError if n.nil? || 0 > n
+    return 1 if n == 0 || n == 1
+    return fib(n - 1) + fib(n - 2)
+  rescue => e
+    return e
+  end
+end
+
+=begin
+c. 組み合わせの数の計算
+=end
+def comb(n, r)
+  begin
+    raise ArgumentError if n.nil? || r.nil? || 0 > n || 0 > r || r > n
+    return 1 if r == n || r == 0
+    return comb(n - 1, r) + comb(n - 1, r - 1)
+  rescue => e
+    return e
+  end
+end
+
+=begin
+d. 正の整数 n の 2 進表現
+=end
+def binary(n)
 end
