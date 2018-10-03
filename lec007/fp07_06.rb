2a19f554b45e0401a87fe2e6b7f32242904e8f3e
otake <sheepdogs_2@hotmail.com>
2018-10-03 23:20:18 +0900

    第7講、基数ソートの終了条件を修正

--- lec007/fp07_06.rb
+++ lec007/fp07_06.rb
@@ -2,43 +2,6 @@ require 'benchmark'
 require 'bigdecimal'
 require 'bigdecimal/util'
 
-
-def radixsort(a)
-  begin
-    raise ArgumentError if a.nil?
-    mask = 0b01
-    flg = true
-    right = Array.new
-    left = Array.new
-    #/==============計測開始==============/
-    #time = Benchmark.realtime do
-      while flg == true do
-        flg = false
-        left = []
-        right = []
-        a.each_with_index do |val,i|
-          if  val & mask == 0 then
-            left.push(val)
-          else
-            right.push(val)
-            flg = true
-          end
-        end
-        a = []
-        left.concat(right)
-        a = left.dup
-        mask = mask << 1
-      end
-    #end
-    #puts("処理時間 #{time}s")
-    #/=============計測終了===============/
-    return a
-  rescue => e
-    return e
-  end
-end
-
-=begin
 def radixsort(a)
   begin
     raise ArgumentError if a.nil?
@@ -47,21 +10,22 @@ def radixsort(a)
     #/==============計測開始==============/
     #time = Benchmark.realtime do
       while flg == true do
-        flg = false
         left = Array.new
         right = Array.new
-        a.each_with_index do |val,i|
+        # 配列の最大値を取得し終了条件を導出する事とする
+        max_num = a.max
+        a.each_with_index do |val,j|
           if  val & mask == 0 then
             left.push(val)
           else
             right.push(val)
-            flg = true
           end
         end
         a = Array.new
         left.concat(right)
         a = left.dup
         mask = mask << 1
+        flg = false if mask > max_num
       end
     #end
     #puts("処理時間 #{time}s")
@@ -71,4 +35,3 @@ def radixsort(a)
     return e
   end
 end
-=end
