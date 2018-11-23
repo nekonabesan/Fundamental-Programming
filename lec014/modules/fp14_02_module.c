fcc66e8fe183116bb4e1df6da4e0dd406c486977
nekonabesan <sheepdogs_2@hotmail.com>
2018-11-24 08:31:41 +0900

    第１３講、第１４講、ディレクトリ作成

---
+++ lec014/modules/fp14_02_module.c
@@ -0,0 +1,21 @@
+// 演習 2 上の例題をそのまま動かし、暗い色ができることを確認しなさい。
+// OK なら次のような関数を作ってみなさい。
+
+
+// a. 色を明るく変化させる関数 void makebrighter(struct color *p)。
+
+
+// b. 先の演習のreversecolorと同じ変化を施す関数void makereverse(struct color *p)。
+
+
+// c. 先の演習のrot1color、rot2color と同様の変化をおこなう関数
+// void makerot1(structcolor *p)、void makerot2(struct color *p)。
+
+
+// d. RGB 値の増分 (マイナスでもよい) を受け取り、その分だけそれぞれの成分を増やす関数
+// void addtocolor(struct color *p, int dr, int dg, int db)。
+
+// e. RGB 値それぞれに-10～10 の範囲のランダムな値を足すことで元とちょっとだけ違う色に
+// する関数 void varcolor(struct color *p)。
+
+// f. その他色の構造体のアドレスを受け取り、好きな変化を施す関数。
